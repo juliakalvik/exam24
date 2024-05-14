172010cdaa75b19a410573e5cc812d8ff4acce73
Julia Kalvik <julia.kalvik@yahoo.no>
2024-05-14 22:33:37 +0200

    Updated design for venues card

--- src/components/venues/index.jsx
+++ src/components/venues/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { Link } from "@tanstack/react-router";
 
@@ -9,9 +9,11 @@ const FetchVenues = ({ searchQuery }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://v2.api.noroff.dev/holidaze/venues?_owner=true");
+        const response = await fetch(
+          "https://v2.api.noroff.dev/holidaze/venues?_owner=true"
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
         const data = await response.json();
         setVenues(data.data);
@@ -24,9 +26,10 @@ const FetchVenues = ({ searchQuery }) => {
   }, []);
 
   useEffect(() => {
-    
-    const filtered = venues.filter(venue =>
-      venue.name.toLowerCase().includes(searchQuery.toLowerCase()) || venue.location.city === searchQuery
+    const filtered = venues.filter(
+      (venue) =>
+        venue.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        venue.location.city === searchQuery
     );
     setFilteredVenues(filtered);
   }, [searchQuery, venues]);
@@ -36,20 +39,23 @@ const FetchVenues = ({ searchQuery }) => {
       {filteredVenues.map((data) => (
         <div key={data.id}>
           <div key={data.id} className="venuescard">
-            <Link to={`../venueDetails?venueId=${data.id}`}> Click here </Link>
-            <div className="cardflex">
-              <h2>{data.name}</h2>
-              <p>Price per day: {data.price}</p>
-              <p>
-                Owner:{" "}
-                <a href={`../ownerprofile/${data.owner.name}`}>
-                  {data.owner.name}
-                </a>
-              </p>
-
+            <Link to={`../venueDetails?venueId=${data.id}`}>
+              {" "}
               {data.media && data.media.length > 0 && (
                 <img src={data.media[0].url} alt="Venue" />
-              )}
+              )}{" "}
+            </Link>
+            <div className="cardflex">
+              <h2>{data.name}</h2>
+              <div className="ownerflex">
+                <p>Price per night: {data.price}</p>
+                <p>
+                  Owner:{" "}
+                  <a href={`../ownerprofile/${data.owner.name}`}>
+                    {data.owner.name}
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
