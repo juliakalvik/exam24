667f3c4488974317c6a0e11889b88fdaf00313b0
Julia Kalvik <julia.kalvik@yahoo.no>
2024-05-15 15:51:31 +0200

    Updates to hero section

--- src/components/homeHero/index.jsx
+++ src/components/homeHero/index.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
-const HomeHero = ({ setSearchQuery, handleCityButtonClick, handleClearFilter }) => {
+const HomeHero = ({
+  setSearchQuery,
+  handleCityButtonClick,
+  handleClearFilter,
+}) => {
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
   return (
-    <header>
-      <h1>Venues for any event or happening</h1>
-      <p>Weddings, birthday parties, proms</p>
-      <div className="search-bar">
-        <input type="text" placeholder="Search by name" onChange={handleSearchChange} />
-      </div>
-      <div className="city-buttons">
-        <button onClick={() => handleCityButtonClick('Oslo')}>Oslo</button>
-        <button onClick={() => handleCityButtonClick('Vilajoyhosa')}>Vilajoyhosa</button>
-        <button onClick={() => handleCityButtonClick('Kristiansand')}>Kristiansand</button>
-        <button onClick={() => handleCityButtonClick('Berlin')}>Berlin</button>
-        <button onClick={() => handleCityButtonClick('Bergen')}>Bergen</button>
-        <button onClick={handleClearFilter}>Clear Filter</button>
-      </div>
-    </header>
+    <header className="herotext">
+  <h1>Find your dream destination</h1>
+  <p>Where will your next adventure be?</p>
+  <div className="search-bar">
+    <input
+      type="text"
+      placeholder="Search by name"
+      onChange={handleSearchChange}
+    /> 
+    <i className="fa-solid fa-magnifying-glass" aria-hidden="true"></i>
+  </div>
+  <div className="buttonssection">
+    <p className="popular">Most popular:</p>
+    <div className="city-buttons">
+      <button onClick={() => handleCityButtonClick("Oslo")}>Oslo</button>
+      <button onClick={() => handleCityButtonClick("Vilajoyhosa")}>
+        Vilajoyhosa
+      </button>
+      <button onClick={() => handleCityButtonClick("Kristiansand")}>
+        Kristiansand
+      </button>
+      <button onClick={() => handleCityButtonClick("Berlin")}>Berlin</button>
+      <button onClick={() => handleCityButtonClick("Bergen")}>Bergen</button>
+      <button onClick={handleClearFilter}>Clear Filter</button>
+    </div>
+  </div>
+</header>
+
   );
 };
 
